f1bdc105f77459886e65286ac4590ecffef1323c
William Rivera <william.r.2048@gmail.com>
2023-06-15 20:46:44 -0400

    wrote code to initialize detailed and summary tables

--- query_files/investigative_queries.sql
+++ query_files/investigative_queries.sql
@@ -92,6 +92,7 @@ FROM rental ) t;
 -- first entry was 2005-5-24
 -- can use just last quarter of 2005 
 -- 12, 11, 10
+SELECT DATE('2005-08-23') - DATE('2005-05-24');
 
 
 SELECT * 
@@ -114,6 +115,11 @@ WHERE
 	rental_date >= '2005-01-01' AND rental_date <= '2005-12-31'
 ORDER BY 2 DESC;
 
+-- could be useful for getting new data
+SELECT *
+FROM rental
+WHERE rental_date BETWEEN (SELECT MAX(rental_date) - INTERVAL '180 days' FROM rental)  AND (SELECT MAX(rental_date) FROM rental)
+AND return_date IS NOT NULL;
 
 
 SELECT * 
@@ -141,7 +147,271 @@ side question:
 - how late were they returned, on average?
 
 - how many if at all, were never returned?
+
+Requirements:
+tables:
+detailed, summary table
+
+trigger:
+trigger to update summary as detailed is modified
+
+stored procedure:
+stored procedure to truncate both tables and extract new data
+
+function:
+function to modify at least one column in the detailed table
+
+automation:
+repeat cycle
+
+Plan:
+1. init detailed 
+2. function to modify detailed column
+3. init summary
+4. trigger summary
+5. stored procedure to:
+		- truncate tables
+		A populate detailed
+			-- insert into detailed
+		- perform aggrigations
+		B populate summary
+			-- insert into summary
+6. automate with events
 */
 
+-- original
+SELECT 
+	rental.rental_id,
+	store_id,
+	category.category_id,
+	f.film_id,
+	f.title,
+	category.name AS genre_name,
+	f.release_year,
+	f.rental_duration,
+	f.rental_rate,
+	f.length,
+	f.rating,
+	rental_date, 
+	return_date, 
+	(return_date - rental_date) as time_rented,
+	EXTRACT(DAY FROM return_date - rental_date) AS test,
+	payment.amount AS amount_paid
+FROM category
+	JOIN film_category ON category.category_id = film_category.category_id
+	JOIN film f ON f.film_id = film_category.film_id
+	JOIN inventory ON f.film_id = inventory.film_id
+ 	JOIN rental ON inventory.inventory_id = rental.inventory_id
+	JOIN payment ON rental.rental_id = payment.rental_id
+	WHERE return_date - rental_date IS NOT NULL 
+ORDER BY title ASC, time_rented DESC;
 
+----- INITIAL TABLES
+-- DETAILED
+-- 91 days
+-- dates presented are the most solid chunks of time
+
+
+-- INITIAL TABLE, CREATE DETAILED
+DROP TABLE IF EXISTS quarterly_detailed_report;
+
+CREATE TABLE quarterly_detailed_report AS 
+	SELECT 
+		rental.rental_id,
+		store_id,
+		category.category_id,
+		f.film_id,
+		f.title,
+		category.name AS genre_name,
+		f.release_year,
+		f.length,
+		f.rating,
+		f.rental_rate,
+		payment.amount AS amount_paid,
+		f.rental_duration,
+		return_date - rental_date AS days_rented
+	FROM category
+		JOIN film_category ON category.category_id = film_category.category_id
+		JOIN film f ON f.film_id = film_category.film_id
+		JOIN inventory ON f.film_id = inventory.film_id
+		JOIN rental ON inventory.inventory_id = rental.inventory_id
+		JOIN payment ON rental.rental_id = payment.rental_id
+	WHERE 
+		return_date BETWEEN '2005-05-24' AND '2005-08-23'
+		AND
+		return_date IS NOT NULL;
+
+SELECT *, rented_days_elapsed(days_rented) 
+FROM quarterly_detailed_report;
+
+
+-- CREATE FUNCTION 
+DROP FUNCTION IF EXISTS rented_days_elapsed;	
+	
+-- function to return days rented, summing up all days, hours, and minutes with
+-- respect to days, and then rounding to a single digit. 
+CREATE FUNCTION rented_days_elapsed(
+	days_rented INTERVAL
+) 
+RETURNS SMALLINT AS
+$$
+	DECLARE days_elapsed SMALLINT;
+BEGIN	
+	days_elapsed = ROUND(
+		(EXTRACT (DAY FROM days_rented) + 
+		 EXTRACT (HOUR FROM days_rented)/24 + 
+		 EXTRACT(MINUTE FROM days_rented)/(24 * 60)), 0
+		);
+	RETURN days_elapsed;
+END;
+$$
+LANGUAGE plpgsql;	
+
+-- TEST FUNCTION 
+SELECT *, rented_days_elapsed(days_rented) 
+FROM quarterly_detailed_report;
+
+-- MODIFY DAYS_RENTED TO GO FROM INTERVAL TO SMALLINT
+
+-- because the column is an entirely *different* datatype, 
+-- we have to alter the columns datatype and update values 
+-- simultaneously, using the following syntax.
+
+ALTER TABLE quarterly_detailed_report
+	ALTER COLUMN days_rented TYPE SMALLINT
+	USING rented_days_elapsed(days_rented);
+
+-- VIEW DATA
+SELECT *
+FROM quarterly_detailed_report;
+
+-- CLEANING DETAILED DATA			
+
+-- there is a mistake in the dataset where multiple rental_id's populate for one
+-- specific id, which should not be the case. Each rental_id should be unique,
+-- even if a single customer rents multiple DVD's in a given day. 
+-- The duplicate is from the payments window. 
+-- The best course of action is to delete all of the data related to rental_id 4591,
+-- because we can not confirm the accuracy of any given row related to that rental id.
+
+
+-- delete junk data
+DELETE FROM quarterly_detailed_report
+WHERE rental_id = 4591;
+
+-- proof all rentals are supposed to be unique even if customer rents same day 
+SELECT COUNT(*), customer_id, rental_date
+FROM rental
+GROUP BY customer_id, rental_date
+HAVING COUNT(*) > 1;
+
+-- duplicate entries in payment table only
+SELECT COUNT(*) FROM payment
+GROUP BY rental_id
+HAVING COUNT(*) > 1;
+
+-- mistake in data
+SELECT * FROM film
+JOIN inventory USING(film_id)
+JOIN rental USING(inventory_id)
+JOIN payment USING(rental_id)
+WHERE rental_id = 4591;
+
+-- checking to make sure 4591 is the only offender 
+SELECT rental_id, COUNT(*) AS total
+FROM quarterly_detailed_report
+GROUP BY rental_id
+HAVING COUNT(*) > 1;
+
+
+SELECT * FROM quarterly_detailed_report
+WHERE days_rented IS NULL;
+
+-- SUMMARY TABLE
+-- most popular films
+-- average rent times
+-- number of films returned late
+-- average amount of days returned late
+
+SELECT *, days_rented - rental_duration FROM quarterly_detailed_report;
+
+CREATE TABLE quarterly_summary AS 
+WITH cte AS (
+    SELECT
+        q.film_id,
+		q.genre_name,
+		q.title,
+		q.rental_duration,
+        COUNT(q.film_id) AS times_rented,
+        COUNT(*) FILTER (WHERE q.rental_rate < q.amount_paid) AS times_returned_late,
+        ROUND(AVG(q.days_rented), 0) AS average_days_rented,
+		COALESCE(ROUND(AVG(q.days_rented - q.rental_duration)  FILTER (WHERE q.rental_rate < q.amount_paid), 0), 0) AS average_days_returned_late
+    FROM
+        quarterly_detailed_report q
+    GROUP BY
+        q.film_id, q.title, q.genre_name, q.rental_duration
+)
+
+SELECT 
+	*, 
+    ROUND(CASE 
+		  WHEN cte.times_returned_late != 0 THEN (cte.times_returned_late::NUMERIC / cte.times_rented) * 100 
+		  ELSE 0 
+		  END, 2) AS percentage_returned_late
+
+	  
+FROM cte
+ORDER BY 5 DESC, 3 ASC;
+
+-- TESTING SUMMARY TABLE
+SELECT * FROM quarterly_summary;
+
+
+
+-- most popular films, average rent times
+
+-- 'ROUGH DRAFT'
+-- very in-effecient but just getting ideas 
+/*
+SELECT 
+	ROUND(CASE 
+		  	WHEN times_returned_late != 0 THEN (times_returned_late::NUMERIC / times_rented)
+		  	ELSE 0
+		  END, 2) * 100 AS percentage_returned_late, *
+FROM (
+SELECT 
+	COUNT(film_id)  times_rented,
+	(SELECT COUNT(*) FROM quarterly_detailed_report  WHERE rental_rate < amount_paid AND film_id = q.film_id) times_returned_late,
+	(SELECT ROUND(AVG(days_rented - rental_duration::NUMERIC), 0) FROM quarterly_detailed_report WHERE rental_rate < amount_paid AND film_id = q.film_id) average_days_returned_late,
+	ROUND(AVG(days_rented), 0) average_days_rented,
+	ROUND(AVG(days_rented), 0)
+	title,
+	genre_name,
+	release_year,
+	length,
+	rating
+FROM quarterly_detailed_report q
+GROUP BY film_id, title, genre_name, release_year, length, rating ) s ; */
+	
+
+
+---
+
+
+
+SELECT * FROM quarterly_detailed_report
+WHERE film_id = 88;
+
+
+
+
+-- number of films returned late per film
+SELECT 
+	film_id, 
+	COUNT(*) times_film_returned_late,
+	title,
+	ROUND(AVG(days_rented - rental_duration), 0) average_days_returned_late
+FROM quarterly_detailed_report q
+WHERE rental_rate < amount_paid
+GROUP BY film_id, title;
 
